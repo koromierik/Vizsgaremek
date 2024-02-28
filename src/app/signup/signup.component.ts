dd46e725dfff5430a31e2b69d23cb8b7ddbc79ac
KoromiErik <1erikmark@gmail.com>
2024-02-28 20:16:30 +0100

    folytatas

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -3,22 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.css'
+  styleUrls: ['./signup.component.css']
 })
 export class SignupComponent {
-      userData = {
-      username: '',
-      email: '',
-      password: '',
-      confirmPassword: ''
-    };
-  
-    onSubmit() {
-      if (this.userData.password !== this.userData.confirmPassword) {
-        alert('A jelszavak nem egyeznek meg!');
-        return;
-      }
-
-      console.log('A regisztráció sikeres!' + this.userData.username + '!');
-    }
-  }
+ 
+}
