337fd3639c1b9e449ffd6d72444a2b499313a322
KoromiErik <1erikmark@gmail.com>
2024-02-29 11:25:02 +0100

    profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,19 +4,19 @@ import { HomeComponent } from './components/home/home.component';
 import { AddMovieComponent } from './components/add-movie/add-movie.component';
 import { MovieListComponent } from './components/movie-list/movie-list.component';
 import { MovieRatingComponent } from './components/movie-rating/movie-rating.component';
-import { SignupComponent } from './components/signup/signup.component';
-import { UserProfileComponent } from './components/user-profile/user-profile.component';
 import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 const routes: Routes = [
   { path : "home", component: HomeComponent },
   { path : "addmovie", component: AddMovieComponent },
   { path : "movielist", component: MovieListComponent },
   { path : "movierating", component: MovieRatingComponent },
-  { path : "signup", component: SignupComponent },
-  { path : "user", component: UserProfileComponent },
   { path : "login", component: LoginComponent },
-
+  { path : "signup", component: SignupComponent },
+  { path : "profile", component: ProfileComponent },
+  
   { path: " ", component: HomeComponent },
   { path: "**", component: HomeComponent },
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,13 @@ import { AddMovieComponent } from './components/add-movie/add-movie.component';
 import { MovieListComponent } from './components/movie-list/movie-list.component';
 import { MovieRatingComponent } from './components/movie-rating/movie-rating.component';
 import { NavComponent } from './components/nav/nav.component';
+import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
-import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { ProfileComponent } from './components/profile/profile.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { LoginComponent } from './components/login/login.component';
+
 
 @NgModule({
   declarations: [
@@ -23,16 +24,15 @@ import { LoginComponent } from './components/login/login.component';
     AddMovieComponent,
     MovieListComponent,
     MovieRatingComponent,
+    LoginComponent,
     NavComponent,
     SignupComponent,
-    UserProfileComponent,
+    ProfileComponent,
     FooterComponent,
-    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    
     NgbModule,
     HttpClientModule,
     FormsModule
