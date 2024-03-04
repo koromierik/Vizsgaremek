91e53488fd5357f8dd6bd705115a9e3d66d47090
KoromiErik <1erikmark@gmail.com>
2024-03-04 08:16:22 +0100

    profile

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
+  username: string = 'johndoe';
+  email: string = 'johndoe@example.com';
 
+  editedUsername: string = '';
+  editedEmail: string = '';
+  isEditProfileModalOpen: boolean = false;
+
+  editProfile() {
+    this.isEditProfileModalOpen = true;
+    this.editedUsername = this.username;
+    this.editedEmail = this.email;
+  }
+
+  closeEditProfileModal() {
+    this.isEditProfileModalOpen = false;
+  }
+
+  saveChanges() {
+    this.username = this.editedUsername;
+    this.email = this.editedEmail;
+    this.isEditProfileModalOpen = false;
+  }
+
+  logout() {
+  
+  }
 }
