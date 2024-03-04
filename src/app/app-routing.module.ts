44b88d62e7b732a9681f2651eaf1ef9cdf724b71
KoromiErik <1erikmark@gmail.com>
2024-03-04 11:01:05 +0100

    navbar+movielistcomponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { AddMovieComponent } from './components/add-movie/add-movie.component';
 import { MovieListComponent } from './components/movie-list/movie-list.component';
 import { MovieRatingComponent } from './components/movie-rating/movie-rating.component';
 import { LoginComponent } from './components/login/login.component';
@@ -10,7 +9,6 @@ import { ProfileComponent } from './components/profile/profile.component';
 
 const routes: Routes = [
   { path : "home", component: HomeComponent },
-  { path : "addmovie", component: AddMovieComponent },
   { path : "movielist", component: MovieListComponent },
   { path : "movierating", component: MovieRatingComponent },
   { path : "login", component: LoginComponent },
