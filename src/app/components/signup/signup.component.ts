4a4d0c0db04f18e114b6387db248c69ddf8a7123
KoromiErik <1erikmark@gmail.com>
2024-03-05 17:29:59 +0100

    signup

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -7,11 +8,59 @@ import { AuthService } from '../../services/auth.service';
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
-  name: string = '';
-  email: string = '';
-  password: string = '';
-  password_confirmation: string = '';
-  birthdate: string = '';
+  
+  constructor(private authService: AuthService, private router: Router) { }
 
-  constructor(private authService: AuthService) { }
+  name: any;
+  email: any;
+  password: any;
+  password_confirmation: any;
+  birthdate:  Date = new Date();
+
+  isValidEmail(email: string): boolean {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+  }
+
+  isFormValid(): boolean {
+    const isNameValid = !!this.name;
+    const isEmailValid = this.isValidEmail(this.email);
+    const isPasswordValid = !!this.password && this.password.length >= 6;
+    const isPasswordConfirmationValid = this.password_confirmation === this.password;
+    const isBirthDateValid = !!this.birthdate;
+   
+    return (
+      isNameValid &&
+      isEmailValid &&
+      isPasswordValid &&
+      isPasswordConfirmationValid &&
+      isBirthDateValid
+    );
+  }
+    signUp() {
+      if (this.isFormValid()) {
+        this.authService.register(this.email, this.name, this.password, this.password_confirmation, this.birthdate).subscribe(
+          (response: any) => {
+            const email = this.email;
+            const password = this.password;
+            const loginObj = { email, password}
+           
+            this.authService.login(loginObj).subscribe((res: any) => {
+           
+              if (res) {
+                sessionStorage.setItem("token", res.data.token);
+                sessionStorage.setItem("id", res.data.id);
+                this.router.navigateByUrl("/home");
+              }
+              else {
+                alert("Sikertelen bejelentkezés!");
+              }
+            })
+          },
+          (error: any) => {
+            console.error('Regisztrációs hiba:', error);
+            alert('Ezzel az emaillel már van regisztráció!');
+          }
+      );  
+    }
+  }
 }
