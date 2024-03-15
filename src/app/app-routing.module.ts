19f87f8f4242962dd534252b4df2fae48a39061e
KoromiErik <1erikmark@gmail.com>
2024-03-15 14:01:47 +0100

    guarding

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,7 +16,7 @@ const routes: Routes = [
   { path : "login", component: LoginComponent },
   { path : "signup", component: SignupComponent },
   { path : "profile", component: ProfileComponent },
-  { path: "users"}
+  { path: "users"},
   { path : "admin", component: AdminComponent, },
 
   { path: " ", component: HomeComponent },
