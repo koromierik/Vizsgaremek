b0992099c110b7b36d90eb57656b1bcbf15d69b2
KoromiErik <1erikmark@gmail.com>
2024-03-18 15:35:54 +0100

    addmovie admin

--- src/app/components/add-movie/add-movie.component.ts
+++ src/app/components/add-movie/add-movie.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AdminService } from '../../services/admin.service';
 
 
 @Component({
@@ -7,20 +8,56 @@ import { Component } from '@angular/core';
   styleUrl: './add-movie.component.css'
 })
 export class AddMovieComponent {
-  movieTitle: string = '';
-  movieDescription: string = '';
+  title: string = '';
+  releaseYear: number = 0;
+  updateTitle: string = '';
+  updateReleaseYear: number = 0;
+  deleteId: number = 0;
 
-  constructor() { }
+  constructor(private adminService: AdminService) { }
 
-  addMovie() {
-    // Itt lehetőség van a film hozzáadásának logikáját megvalósítani
-    // Például a movieTitle és movieDescription értékeit elküldhetjük a szerverre,
-    // ahol létrehozzuk a film objektumot és hozzáadjuk az adatbázishoz.
-    console.log('Hozzáadott film címe:', this.movieTitle);
-    console.log('Hozzáadott film leírása:', this.movieDescription);
-    // További logika a film hozzáadásához
-    // Például: this.movieService.addMovie(this.movieTitle, this.movieDescription);
-    // Ahol movieService egy szolgáltatás lenne, ami felelős a filmek kezeléséért
-    // (hozzáadás, szerkesztés, törlés stb.)
+  addMovie(): void {
+    if (this.title && this.releaseYear) {
+      this.adminService.addMovie(this.title, this.releaseYear).subscribe(
+        () => {
+          console.log('Film hozzáadva!');
+        },
+        (error) => {
+          console.error('Hiba történt a film hozzáadása során:', error);
+        }
+      );
+    } else {
+      console.error('Hiányzó adatok. Kérjük, töltse ki az összes mezőt.');
+    }
+  }
+
+  updateMovie(): void {
+    if (this.updateTitle && this.updateReleaseYear) {
+      this.adminService.updateMovie(this.deleteId, this.updateTitle, this.updateReleaseYear).subscribe(
+        () => {
+          console.log('Film frissítve!');
+        },
+        (error) => {
+          console.error('Hiba történt a film frissítése során:', error);
+        }
+      );
+    } else {
+      console.error('Hiányzó adatok. Kérjük, töltse ki az összes mezőt.');
+    }
+  }
+
+  deleteMovie(): void {
+    if (this.deleteId) {
+      this.adminService.deleteMovie(this.deleteId).subscribe(
+        () => {
+          console.log('Film törölve!');
+        },
+        (error) => {
+          console.error('Hiba történt a film törlése során:', error);
+        }
+      );
+    } else {
+      console.error('Hiányzó adatok. Kérjük, töltse ki az összes mezőt.');
+    }
   }
 }
