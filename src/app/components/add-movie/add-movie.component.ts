f9554625ab80d6983095edf105e707750b0428be
KoromiErik <1erikmark@gmail.com>
2024-03-19 18:26:26 +0100

    addmovie

--- src/app/components/add-movie/add-movie.component.ts
+++ src/app/components/add-movie/add-movie.component.ts
@@ -16,52 +16,80 @@
     updateReleaseYear: number = 0;
     updateDescription: string = '';
     deleteId: number = 0;
-
+  
     constructor(private adminService: AdminService) { }
-
+  
     addMovie(): void {
       if (this.title && this.releaseYear && this.description && this.review) {
         this.adminService.addMovie(this.title, this.releaseYear, this.description, this.review).subscribe(
           () => {
             console.log('Film hozzáadva!');
+            alert('Film hozzáadva!');
+            this.resetForm();
           },
           (error) => {
             console.error('Hiba történt a film hozzáadása során:', error);
+            alert('Hiba történt a film hozzáadása során!');
           }
         );
       } else {
         console.error('Hiányzó adatok. Kérjük, töltse ki az összes mezőt.');
+        alert('Hiányzó adatok. Kérjük, töltse ki az összes mezőt.');
       }
     }
-
+  
     updateMovie(): void {
-      if (this.updateTitle && this.updateReleaseYear && this.updateDescription && this.review) {
+      if (this.updateTitle && this.updateReleaseYear && this.updateDescription) {
         this.adminService.updateMovie(this.deleteId, this.updateTitle, this.updateReleaseYear, this.updateDescription, this.review).subscribe(
           () => {
             console.log('Film frissítve!');
-
+            alert('Film frissítve!');
+            this.resetUpdateForm();
           },
           (error) => {
             console.error('Hiba történt a film frissítése során:', error);
+            alert('Hiba történt a film frissítése során!');
           }
         );
       } else {
         console.error('Hiányzó adatok. Kérjük, töltse ki az összes mezőt.');
+        alert('Hiányzó adatok. Kérjük, töltse ki az összes mezőt.');
       }
     }
-
+  
     deleteMovie(): void {
       if (this.deleteId) {
         this.adminService.deleteMovie(this.deleteId).subscribe(
           () => {
             console.log('Film törölve!');
+            alert('Film törölve!');
+            this.resetDeleteForm();
           },
           (error) => {
             console.error('Hiba történt a film törlése során:', error);
+            alert('Hiba történt a film törlése során!');
           }
         );
       } else {
         console.error('Hiányzó adatok. Kérjük, töltse ki az összes mezőt.');
+        alert('Hiányzó adatok. Kérjük, töltse ki az összes mezőt.');
       }
+    }
+  
+    private resetForm(): void {
+      this.title = '';
+      this.releaseYear = 0;
+      this.description = '';
+      this.review = '';
+    }
+  
+    private resetUpdateForm(): void {
+      this.updateTitle = '';
+      this.updateReleaseYear = 0;
+      this.updateDescription = '';
+    }
+  
+    private resetDeleteForm(): void {
+      this.deleteId = 0;
     }
   }
