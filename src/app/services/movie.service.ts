5a6d1439e6d773b6daa611375ef032131ca05176
KoromiErik <1erikmark@gmail.com>
2024-03-19 19:56:54 +0100

    services update

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,9 +1,39 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
 
-  constructor() { }
+  private url = "http://127.0.0.1:8000/api";
+  constructor(private http: HttpClient) { }
+
+  addMovie(title: string, releaseYear: number, description: string, review: string): Observable<any> {
+    const movie = {
+      title,
+      releaseYear,
+      description,
+      review
+    };
+    return this.http.post<any>(`${this.url}/addmovie`, movie);
+  }
+  getMovie(id: number): Observable<any> {
+    return this.http.get<any>(`${this.url}/getmovie?id=${id}`);
+  }
+  
+  updateMovie(id: number, title: string, releaseYear: number, description: string, review: string ): Observable<any> {
+    const movie = {
+      id,
+      title,
+      releaseYear,
+      description,
+      review
+    };
+    return this.http.put<any>(`${this.url}/updatemovie`, movie);
+  }
+  deleteMovie(id: number): Observable<any> {
+    return this.http.delete<any>(`${this.url}/deletemovie?id=${id}`);
+  }
 }
