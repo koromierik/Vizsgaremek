b40a89ba41e1c2f50f88860d199232285a0b8020
KoromiErik <1erikmark@gmail.com>
2024-03-19 10:11:04 +0100

    routes

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -7,4 +9,36 @@ import { Component } from '@angular/core';
 })
 export class NavComponent {
 
+  constructor(private router:Router, protected auth:AuthService) { }
+  
+  loggedin(){
+    let token = sessionStorage.getItem("token");
+    if(token){
+      return true
+    }
+    else{
+      return false
+    }
+  }
+  admin(){
+    if(sessionStorage.getItem("role")=="admin"){
+      return true
+    }
+    else{
+      return false
+    }
+  }
+  logout(){
+    const token = sessionStorage.getItem("token");
+    if(token){
+      this.auth.logout(token)
+      sessionStorage.removeItem("token")
+      sessionStorage.removeItem("id")
+      sessionStorage.removeItem("role")
+      this.router.navigateByUrl("/home")
+    }
+    else{
+      alert("Már ki vagy jelentkezve!")
+    }
+  }
 }
